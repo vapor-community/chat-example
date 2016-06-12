d6e24b997d0a46f9dfff0ba3bf8992f8f9a4d70c
Logan Wright <logan.william.wright@gmail.com>
2016-06-12 17:59:38 -0400

    cleanup extranneous main

--- App/main.swift
+++ App/main.swift
@@ -1,7 +1,5 @@
 import Vapor
 
-var stargazers = 0
-var activeSockets: [WebSocket] = []
 View.renderers["html"] = HTMLRender()
 
 let app = Application(workDir: workDir)
@@ -9,11 +7,11 @@ let app = Application(workDir: workDir)
 // MARK: Visit
 
 app.get { req in
-    var msg = JSON.object([:])
-    msg["type"] = JSON("visit")
-    msg["ip"] = req.headers["X-Forwarded-For"].flatMap { JSON($0) }
-    try activeSockets.forEach { ws in try ws.send(msg) }
 
+    /*
+     STYLE SOURCE:
+     http://codepen.io/supah/pen/jqOBqp?utm_source=bypeople
+     */
     return try app.view("welcome.html")
 }
 
@@ -35,40 +33,6 @@ app.get("chat") { req in
         }
         ws.onClose = { ws, _, _, _ in
             chatters = chatters.filter { $0 !== ws }
-        }
-    }
-}
-
-/*
- STYLE SOURCE:
- http://codepen.io/supah/pen/jqOBqp?utm_source=bypeople
- */
-
-// MARK: Web Hook
-
-app.post("gh-webhook") { req in
-    guard
-        let stars = req.data["repository", "stargazers_count"].int,
-        let repo = req.data["repository", "name"]?.string
-        else { return Response(status: .ok) } // ok to gh ping
-    stargazers = stars
-
-    var msg = JSON.object([:])
-    msg["type"] = JSON("stars")
-    msg["count"] = JSON(stars)
-    msg["repo-name"] = JSON(repo)
-    try activeSockets.forEach { ws in try ws.send(msg) }
-
-    return Response(status: .ok)
-}
-
-// MARK: Socket Listeners
-
-app.get("updates") { req in
-    return try req.upgradeToWebSocket { ws in
-        activeSockets.append(ws)
-        ws.onClose = { _ in
-            activeSockets = activeSockets.filter { $0 !== ws }
         }
     }
 }
