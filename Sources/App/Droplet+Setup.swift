242007b3748ddb0e4f87484a68b8346033f43871
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-10-02 00:51:51 +0200

    Add periodic ping to avoid disconnection after 30s

--- Sources/App/Droplet+Setup.swift
+++ Sources/App/Droplet+Setup.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Foundation
 
 let room = Room()
 
@@ -9,10 +10,16 @@ extension Droplet {
         }
         
         socket("chat") { req, ws in
+            var pingTimer: DispatchSourceTimer? = nil
             var username: String? = nil
-            
+
+            pingTimer = DispatchSource.makeTimerSource()
+            pingTimer?.scheduleRepeating(deadline: .now(), interval: .seconds(25))
+            pingTimer?.setEventHandler { try? ws.ping() }
+            pingTimer?.resume()
+
             ws.onText = { ws, text in
-                let json = try JSON(bytes: Array(text.utf8))
+                let json = try JSON(bytes: text.makeBytes())
                 
                 if let u = json.object?["username"]?.string {
                     username = u
@@ -26,6 +33,9 @@ extension Droplet {
             }
             
             ws.onClose = { ws, _, _, _ in
+                pingTimer?.cancel()
+                pingTimer = nil
+
                 guard let u = username else {
                     return
                 }
